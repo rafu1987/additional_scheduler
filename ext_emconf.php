66bf804b03e8fd0d52084b6d272ee384a88fbaf7
Raphael Zschorsch <rafu1987@gmail.com>
2017-02-13 21:21:43 +0100

    Update php version requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company'   => '',
     'constraints'      => array(
         'depends'   => array(
-            'php'       => '5.0.0-5.6.99',
+            'php'       => '5.0.0-7.0.99',
             'typo3'     => '6.2.0-7.6.99',
             'scheduler' => ''
         ),
